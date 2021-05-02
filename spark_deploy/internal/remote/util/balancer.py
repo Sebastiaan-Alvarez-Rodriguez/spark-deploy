11a6dd336e77b4f39d47e7e7a97714d0ea3520f5
Sebastiaan Alvarez Rodriguez van Ravensberg <sebastiaanalva@gmail.com>
2021-05-02 13:22:23 +0200

    WIP Base cleanup

--- spark_deploy/internal/remote/util/balancer.py
+++
@@ -1,5 +0,0 @@
-# The simplest load balancer available.
-# Maps every next client to the next server, wrapping around the end of the server list
-# Returns the target for given gid from the server list
-def balance_simple(serverlist, gid):
-    return serverlist[gid % len(serverlist)]
